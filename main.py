9706a294667ea7e801acf3443c2b203170e25d5a
ChaosNuggets <chaosnuggets@gmail.com>
2026-05-08 04:15:04 -0400

    wrote code to create each subfolder

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import re
+from pathlib import Path
 
 def main():
     input_dir, output_dir = read_conf_file()
-    print(input_dir)
-    print(output_dir)
+    write_output(input_dir, output_dir)
 
+# Returns the input_dir and output_dir entries in osu-song-extractor.cfg
 def read_conf_file() -> tuple[str, str]:
     input_dir = ''
     output_dir = ''
@@ -25,6 +26,25 @@ def read_conf_file() -> tuple[str, str]:
     if not input_dir or not output_dir:
         raise KeyError('Please specify both input_dir and output_dir in the configuration file')
     return input_dir, output_dir
+
+def write_output(input_dir: str, output_dir: str) -> None:
+    # A compiled regex pattern for capturing folder ID
+    # into group 1 and everything past that into group 2
+    subfolder_pattern = re.compile(fr'{input_dir}/(\d+)\s*(.*)')
+
+    # Search each subfolder of the songs folder
+    p_in = Path(input_dir)
+    for p_in_sub in p_in.iterdir():
+        if not p_in_sub.is_dir():
+            continue
+
+        # Create the output_subfolder
+        input_subfolder = str(p_in_sub)
+        output_subfolder = f'{output_dir}/' + subfolder_pattern.sub(r'\2 \1', input_subfolder)
+        p_out_sub = Path(output_subfolder)
+        p_out_sub.mkdir(parents=True, exist_ok=True)
+
+        # TODO: code up the rest of the plan lol
 
 if __name__ == '__main__':
     main()
