baa6abdc35eeea7a7051bc82bc955936f69f18d7
ChaosNuggets <chaosnuggets@gmail.com>
2026-05-08 07:57:53 -0400

    dealt with things that would lead to illegal filenames

--- copy_audio.py
+++ copy_audio.py
@@ -8,18 +8,24 @@ version_pat = re.compile(r'^Version:')
 events_pat = re.compile(r'^\[Events\]')
 blank_line_pat = re.compile(r'^\s*$')
 bg_filename_pat = re.compile(r'^0,0,([^,]*),\d+,\d+')
+forbidden_pat = re.compile(r'[<>:"\/\\|?*]')
 
 def copy_audio(p_in_sub: Path, p_out_sub: Path):
     prev_audio_filenames = set()
     for osu_file in list(p_in_sub.glob('*.osu')):
         extract_beatmap_info(osu_file)
 
+# Looks at .osu file and returns audio_filename, title,
+# artist, version, and background filename (if it exists)
 def extract_beatmap_info(osu_file: Path):
     audio_filename = ''
     title = ''
     artist = ''
     version = ''
     bg_filename = ''
+    legal_title = ''
+    legal_artist = ''
+    legal_version = ''
     is_events_section = False
     with open(osu_file, 'r') as file:
         for line in file:
@@ -27,10 +33,13 @@ def extract_beatmap_info(osu_file: Path):
                 audio_filename = audio_filename_pat.sub('', line).strip('\n \"\'')
             elif title_pat.search(line):
                 title = title_pat.sub('', line).strip('\n \"\'')
+                legal_title = forbidden_pat.sub(r'-', title)
             elif artist_pat.search(line):
                 artist = artist_pat.sub('', line).strip('\n \"\'')
+                legal_artist = forbidden_pat.sub(r'-', artist)
             elif version_pat.search(line):
                 version = version_pat.sub('', line).strip('\n \"\'')
+                legal_version = forbidden_pat.sub(r'-', version)
             elif events_pat.search(line):
                 is_events_section = True
             elif blank_line_pat.search(line):
@@ -41,12 +50,21 @@ def extract_beatmap_info(osu_file: Path):
             if audio_filename and title and artist and version and bg_filename:
                 break
 
-    print(audio_filename)
-    print(title)
-    print(artist)
-    print(version)
-    print(bg_filename)
+    # print(audio_filename)
+    # print(title)
+    # print(artist)
+    # print(version)
+    # print(bg_filename)
+    # print(legal_title)
+    # print(legal_artist)
+    # print(legal_version)
+    # print()
+
+    if not audio_filename or not title or not artist or not version:
+        raise KeyError(f"{str(osu_file)} is missing AudioFilename, Title, Artist, or Version")
+
+    return audio_filename, title, artist, version, bg_filename
 
 # Testing code
-p_in_sub = Path('/home/stanley/Music/Songs/2006102 Laur - SYSTEM ERROR/')
+p_in_sub = Path('/home/stanley/Music/Songs/1816915 Sayuri - Hana no Tou/')
 copy_audio(p_in_sub, p_in_sub)
