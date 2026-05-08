88f9eebd90cc6b12e8e3db1ac9188a37726488a7
ChaosNuggets <chaosnuggets@gmail.com>
2026-05-08 07:34:09 -0400

    extracted beatmap info from .osu file

--- copy_audio.py
+++ copy_audio.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 audio_filename_pat = re.compile(r'^AudioFilename:')
 title_pat = re.compile(r'^Title:')
@@ -6,12 +7,12 @@ artist_pat = re.compile(r'^Artist:')
 version_pat = re.compile(r'^Version:')
 events_pat = re.compile(r'^\[Events\]')
 blank_line_pat = re.compile(r'^\s*$')
-bg_filename_pat = re.compile(r'^0,0,([^,]*)')
+bg_filename_pat = re.compile(r'^0,0,([^,]*),\d+,\d+')
 
 def copy_audio(p_in_sub: Path, p_out_sub: Path):
     prev_audio_filenames = set()
     for osu_file in list(p_in_sub.glob('*.osu')):
-        extract_beatmap_info()
+        extract_beatmap_info(osu_file)
 
 def extract_beatmap_info(osu_file: Path):
     audio_filename = ''
@@ -19,7 +20,7 @@ def extract_beatmap_info(osu_file: Path):
     artist = ''
     version = ''
     bg_filename = ''
-    is_events_section = false
+    is_events_section = False
     with open(osu_file, 'r') as file:
         for line in file:
             if audio_filename_pat.search(line):
@@ -31,6 +32,21 @@ def extract_beatmap_info(osu_file: Path):
             elif version_pat.search(line):
                 version = version_pat.sub('', line).strip('\n \"\'')
             elif events_pat.search(line):
-                is
+                is_events_section = True
+            elif blank_line_pat.search(line):
+                is_events_section = False
+            elif is_events_section and bg_filename_pat.search(line):
+                bg_filename = bg_filename_pat.sub(r'\1', line).strip('\n \"\'')
 
+            if audio_filename and title and artist and version and bg_filename:
+                break
 
+    print(audio_filename)
+    print(title)
+    print(artist)
+    print(version)
+    print(bg_filename)
+
+# Testing code
+p_in_sub = Path('/home/stanley/Music/Songs/2006102 Laur - SYSTEM ERROR/')
+copy_audio(p_in_sub, p_in_sub)
